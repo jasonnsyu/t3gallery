75551f88bd908fbd8b363a65bc751c01ab69e36c
jasonnsyu <56001852+jasonnsyu@users.noreply.github.com>
2024-07-19 00:19:39 -0700

    1 hr

--- src/app/_components/topnav.tsx
+++ src/app/_components/topnav.tsx
@@ -1,15 +1,17 @@
 import { SignInButton, SignedOut, SignedIn, UserButton } from "@clerk/nextjs";
+import { UploadButton } from "~/utils/uploadthing";
 
 export function TopNav() {
   return (
     <nav className="flex w-full items-center justify-between border-b  p-4 text-xl font-semibold">
       <div>Gallery</div>
 
-      <div>
+      <div className="flex flex-row">
         <SignedOut>
           <SignInButton />
         </SignedOut>
         <SignedIn>
+          <UploadButton endpoint="imageUploader" />
           <UserButton />
         </SignedIn>
       </div>
