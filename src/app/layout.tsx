9eadec6e778d8825942ec469e4afbcccafd82fc9
jasonnsyu <56001852+jasonnsyu@users.noreply.github.com>
2024-07-19 16:33:59 -0700

    1hr 40min

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,8 +19,10 @@ export const metadata = {
 
 export default function RootLayout({
   children,
+  modal,
 }: {
   children: React.ReactNode;
+  modal: React.ReactNode;
 }) {
   return (
     <ClerkProvider>
@@ -37,6 +39,8 @@ export default function RootLayout({
         <body className={`${GeistSans.variable} flex flex-col gap-4`}>
           <TopNav />
           {children}
+          {modal}
+          <div id="modal-root" />
         </body>
       </html>
     </ClerkProvider>
