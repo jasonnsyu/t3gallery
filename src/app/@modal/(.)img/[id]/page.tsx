dd288dc0202f2db49ad74eec68d2b4c0b05bd0ac
jasonnsyu <56001852+jasonnsyu@users.noreply.github.com>
2024-07-20 17:30:07 -0700

    1hr 47min

--- src/app/@modal/(.)img/[id]/page.tsx
+++ src/app/@modal/(.)img/[id]/page.tsx
@@ -1,7 +1,7 @@
-import { getImage } from "~/server/queries";
 import { Modal } from "./modal";
+import FullPageImageView from "~/components/full-image-page";
 
-export default async function PhotoModal({
+export default function PhotoModal({
   params: { id: photoId },
 }: {
   params: { id: string };
@@ -9,10 +9,9 @@ export default async function PhotoModal({
   const idAsNumber = Number(photoId);
   if (Number.isNaN(idAsNumber)) throw new Error("Invalid photo id");
 
-  const image = await getImage(idAsNumber);
   return (
     <Modal>
-      <img src={image.url} className="w-96" />
+      <FullPageImageView id={idAsNumber} />
     </Modal>
   );
 }
