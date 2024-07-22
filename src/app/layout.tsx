a837b6f83af561d78ee714032fce571ff8343f60
jasonnsyu <56001852+jasonnsyu@users.noreply.github.com>
2024-07-22 15:50:38 -0700

    1hr 55min

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -39,12 +39,10 @@ export default function RootLayout({
            */
           routerConfig={extractRouterConfig(ourFileRouter)}
         />
-        <body className={`font-sans ${inter.variable} dark`}>
-          <div className="grid h-screen grid-rows-[auto,1fr]">
-            <TopNav />
-            <main className="overflow-y-scroll">{children}</main>
-            {modal}
-          </div>
+        <body className={`font-sans ${inter.variable} flex flex-col gap-4`}>
+          <TopNav />
+          <main className="overflow-y-scroll">{children}</main>
+          {modal}
           <div id="modal-root" />
         </body>
       </html>
